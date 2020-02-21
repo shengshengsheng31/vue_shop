085f15488dee469d02644b06d9ca550eddd12650
盛盛盛 <1445089856@qq.com>
2020-02-21 09:41:23 +0800

    '完成订单'

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -16,16 +16,75 @@
           </el-input>
         </el-col>
       </el-row>
+      <!--表格-->
       <el-table :data="orderlist" border stripe>
-      <el-table-column type="index" label="#"></el-table-column>
-      <el-table-column  label="订单编号"  prop="order_number"></el-table-column>
-
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template v-slot="scope">
+            <el-tag type="success" v-if="scope.row.pay_status==='1'">已付款</el-tag>
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template v-slot="scope">{{scope.row.create_time | dataFormat}}</template>
+        </el-table-column>
+        <el-table-column label="操作" min-width="120px">
+          <template v-slot="scope">
+            <el-button type="success" icon="el-icon-edit" size="mini" @click="showBox"></el-button>
+            <el-button type="primary" icon="el-icon-location" size="mini" @click="showProgressBox"></el-button>
+          </template>
+        </el-table-column>
       </el-table>
+      <!--分页-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 3, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
+    <!--对话框修改地址-->
+    <el-dialog title="修改地址" :visible.sync="addressVisible" width="50%" @close="addressDialogClosed">
+      <el-form :model="addressForm" :rules="addressRules" ref="addressFormRef" label-width="100px">
+        <el-form-item label="省市区/县" prop="address1">
+          <el-cascader
+            v-model="addressForm.address1"
+            :options="cityData"
+            :props="addressProps"
+            @change="handleChange"
+          ></el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="addressForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addressVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addressVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!--对话框显示物流-->
+    <el-dialog title="物流进度" :visible.sync="progressVisible" width="50%">
+      <!--时间线-->
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in progressInfo"
+          :key="index"
+          :timestamp="activity.time"
+        >{{activity.context}}</el-timeline-item>
+      </el-timeline>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import cityData from './citydata.js'
 export default {
   data () {
     return {
@@ -35,7 +94,26 @@ export default {
         pagesize: 10
       },
       total: 0,
-      orderlist: []
+      orderlist: [],
+      addressVisible: false,
+      addressForm: {
+        address1: [],
+        address2: ''
+      },
+      addressRules: {
+        address1: [
+          { required: true, message: '选择市区/县', trigger: 'blur' }
+        ],
+        address2: [
+          { required: true, message: '输入地址', trigger: 'blur' }
+        ]
+      },
+      cityData,
+      addressProps: {
+        expandTrigger: 'hover'
+      },
+      progressVisible: false,
+      progressInfo: []
 
     }
   },
@@ -43,13 +121,49 @@ export default {
     this.getOrderList()
   },
   methods: {
+    // 获取订单列表
     async getOrderList () {
       const { data: result } = await this.$http.get('orders', { params: this.queryInfo })
       if (result.meta.status !== 200) return this.$message.error(result.meta.msg)
       this.total = result.data.total
       this.orderlist = result.data.goods
+    },
+    // 分页选择页码大小
+    handleSizeChange (newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    // 分页当前页改变
+    handleCurrentChange (newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    },
+    // 对话框显示修改地址
+    showBox () {
+      this.addressVisible = true
+    },
+    // 对话框关闭时清空
+    addressDialogClosed () {
+      this.$refs.addressFormRef.resetFields()
+    },
+    // 级联选择器选择改变
+    handleChange () {
+
+    },
+    // 对话框显示物流
+    async showProgressBox () {
+      const { data: result } = await this.$http.get(`/kuaidi/1106975712662`)
+      if (result.meta.status !== 200) return this.$message.error(result.meta.msg)
+      this.progressInfo = result.data
+      this.progressVisible = true
+      console.log(this.progressInfo)
     }
+
   }
 }
 </script>
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.el-cascader {
+  width: 100%;
+}
+</style>
